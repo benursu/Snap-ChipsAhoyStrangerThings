5a379f658c1e3125dee5e07031020daae8518185
Ben Ursu <ben.ursu@afrosquared.com>
2025-04-13 11:33:35 -0700

    flip test

--- js/main.js
+++ js/main.js
@@ -642,27 +642,27 @@ unmuteBtn.style.display = 'block';
 var counter = 0;
 
 const changeLens = async () => {
+    await updateCamera();
 
-
-    if(counter == 0){
-        lensId = '43276930875';
-        groupId = 'b3bcab54-2bfe-4b99-93bd-31b106ee6c56';
+    // if(counter == 0){
+    //     lensId = '43276930875';
+    //     groupId = 'b3bcab54-2bfe-4b99-93bd-31b106ee6c56';
     
-    }else if(counter == 1){
-        lensId = '43288930875';
-        groupId = 'b3bcab54-2bfe-4b99-93bd-31b106ee6c56';
+    // }else if(counter == 1){
+    //     lensId = '43288930875';
+    //     groupId = 'b3bcab54-2bfe-4b99-93bd-31b106ee6c56';
     
-        await updateCamera();
+    //     await updateCamera();
 
-    }
-    counter++;
+    // }
+    // counter++;
 
-    lens = await cameraKit.lensRepository.loadLens(
-        lensId,
-        groupId
-    );
+    // lens = await cameraKit.lensRepository.loadLens(
+    //     lensId,
+    //     groupId
+    // );
 
-    await cameraKitApply();
+    // await cameraKitApply();
 
 }
 

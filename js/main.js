5adb2379ccd4c2a0d633fc6884003d5c789ae705
Ben Ursu <ben.ursu@afrosquared.com>
2025-04-15 00:19:58 -0700

    cod update

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ var lensCurrent = 0;
 var lenses = [
     { lensId: 'edddcf23-3903-478f-b6f0-254de4cca564', groupId: 'f4bb30fc-3974-4765-b570-3e25b69a102d', camera: 'back'}, //realtime room
     { lensId: 'c3c869c5-55ff-460b-9294-80dfdbe4e504', groupId: 'f4bb30fc-3974-4765-b570-3e25b69a102d', camera: 'back'}, //360 video
-    { lensId: '1a3c25cd-518c-4da8-a23a-fb8915e6a9f3', groupId: 'f4bb30fc-3974-4765-b570-3e25b69a102d', camera: 'front'}, //cod
+    { lensId: 'c188afd7-b8b2-4687-b9c2-b15a7d13ecd8', groupId: 'f4bb30fc-3974-4765-b570-3e25b69a102d', camera: 'front'}, //cod
     // { lensId: 'ad9c900f-8d73-4b33-b70d-63f8897bf9d5', groupId: 'f4bb30fc-3974-4765-b570-3e25b69a102d', camera: 'front'}, //overlook map (map2d)
     // { lensId: 'e8d471eb-cc90-43d5-85d1-3755da73e257', groupId: 'f4bb30fc-3974-4765-b570-3e25b69a102d', camera: 'back'}, //clock
 ];
@@ -376,7 +376,7 @@ const createStreamSource = async () => {
             {
                 width: { ideal: 1280 },
                 height: { ideal: 720 },
-                // facingMode: 'environment',
+                facingMode: 'environment',
             },
             audio: false,
         }
@@ -384,8 +384,8 @@ const createStreamSource = async () => {
 
     source = createMediaStreamSource(stream, {
         transform: Transform2D.MirrorX,
-        cameraType: 'front',
-        // cameraType: 'back',
+        // cameraType: 'front',
+        cameraType: 'back',
     });
     await cameraKitSession.setSource(source);
 
